8264d40be9cb79c32672a7baa95a48f3fb1ac852
ahadakash <ahadakash@hotmail.com>
2022-03-08 15:49:46 +0600

    enter

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useLocation } from 'react-router';
 import useAuth from '../../Hooks/useAuth';
 
 const Login = () => {
@@ -12,19 +11,19 @@ const Login = () => {
                 {!login && <div className="row mb-3">
                     <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
                     <div className="col-sm-10">
-                        <input type="text" onBlur={handleNameChange} className="form-control" id="inputName" placeholder="Please type your name" required />
+                        <input type="text" onBlur={handleNameChange} className="form-control" id="inputName" placeholder="Please enter your name" required />
                     </div>
                 </div>}
                 <div className="row mb-3">
                     <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
                     <div className="col-sm-10">
-                        <input onBlur={changeEmailInput} type="email" className="form-control" id="inputEmail3" placeholder="Please type your email" required />
+                        <input onBlur={changeEmailInput} type="email" className="form-control" id="inputEmail3" placeholder="Please enter your email" required />
                     </div>
                 </div>
                 <div className="row mb-3">
                     <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                     <div className="col-sm-10">
-                        <input onBlur={changePasswordInput} type="password" className="form-control" id="inputPassword3" placeholder="Please type your password" required />
+                        <input onBlur={changePasswordInput} type="password" className="form-control" id="inputPassword3" placeholder="Please enter your password" required />
                     </div>
                 </div>
 
